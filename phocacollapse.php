b9f5d881df3255a797dc7bb79516e331aeb5a07f
Jan <PhocaCz@users.noreply.github.com>
2021-12-09 01:55:42 +0100

    4.0.0
    4.0.0

--- phocacollapse.php
+++ phocacollapse.php
@@ -9,7 +9,7 @@
 
 
 use Joomla\CMS\Factory;
-use Joomla\Http\Response;
+use Joomla\CMS\Language\Text;
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
 jimport( 'joomla.plugin.plugin' );
@@ -18,131 +18,38 @@ class plgSystemPhocaCollapse extends JPlugin
 {
 
 	public function __construct(& $subject, $config) {
+		$this->loadLanguage('plg_system_phocacollapse');
 		parent::__construct($subject, $config);
 	}
 
 	function onBeforeRender() {
 
-		$app 	= JFactory::getApplication();
+		$app 	= Factory::getApplication();
+		if ($app->getDocument()->getType() !== 'html') {
+			return true;
+		}
 
-
-		$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+		$wa = $app->getDocument()->getWebAssetManager();
 		$wa->registerAndUseScript('plg_system_phocacollapse.phocacollapse', 'media/plg_system_phocacollapse/js/phocacollapse.es6.js', array('version' => 'auto'));
 		$wa->registerAndUseStyle('plg_system_phocacollapse.phocacollapse', 'media/plg_system_phocacollapse/css/phocacollapse.css', array('version' => 'auto'));
 
-		$input 	= $app->input;
-		if ($app->getName() != 'site') {
-			return true;
-		}
-
-		/*$format = JRequest::getWord('format');
-		if ($format=='feed' || $format=='json') {
-			return true;
-		}*/
-		// Check if the highlighter should be activated in this environment.
-		if (JFactory::getDocument()->getType() !== 'html' || $input->get('tmpl', '', 'cmd') === 'component')
-		{
-			return true;
-		}
-
-		$document	= JFactory::getDocument();
-		$prm		= array();
-		$prm['head_custom_code'] 	= $this->params->get('head_custom_code', '');
-
-		$document->addCustomTag($prm['head_custom_code']);
-
 	}
+
 	function onAfterRender() {
 
-		$app 	= JFactory::getApplication();
-
+		$app = Factory::getApplication();
+		if ($app->getDocument()->getType() !== 'html') {
+			return true;
+		}
 
 		$buffer = $app->getBody();
 
 		$from = '<div class="subform-repeatable-wrapper subform-layout">';
-		$to = $from. '<div class="ph-collapse"><a href="#" class="phCollapseClick">Show/Hide</a></div>';
+		$to   = $from . '<div class="ph-collapse"><a href="#" class="phCollapseClick">' . Text::_('PLG_SYSTEM_PHOCACOLLAPSE_SHOW_HIDE') . '</a></div>';
 
 		$buffer = str_replace($from, $to, $buffer);
 
-
-       // $wa = Factory::getApplication()->getDocument()->getWebAssetManager();
-		//$wa->registerAndUseScript('plg_system_phocacollapse.phocacollapse', 'media/plg_system_phocacollapse/js/phocacollapse.es6.js', array('version' => 'auto'));
-
 		$app->setBody($buffer);
-		return true;
-
-		bdump($app);
-		if ($app->getName() != 'site') {
-			return true;
-		}
-
-		$format = JRequest::getWord('format');
-		if ($format=='feed') {
-			return true;
-		}
-
-		$prm	=	array();
-		$head 	= $html = $body = '';
-
-		$prm['head_ga_uaid'] 			= $this->params->get('head_ga_uaid', '');
-		$prm['html_xmlns_tags'] 		= $this->params->get('html_xmlns_tags', '');
-		$prm['body_custom_code'] 		= $this->params->get('body_custom_code', '');
-
-		if ($prm['html_xmlns_tags'] != '') {
-			$html .= str_replace(',', ' ', strip_tags((string)$prm['html_xmlns_tags']));
-			//$html[] = explode(',', strip_tags((string)$prm['html_xmlns_tags']));
-		}
-
-		if ($prm['body_custom_code'] != '') {
-			$body .= $prm['body_custom_code'];
-		}
-
-		if($prm['head_ga_uaid'] != '' ) {
-		/*	$head .=
-'<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push([\'_setAccount\', \''.strip_tags($prm['head_ga_uaid']).'\']);
-  _gaq.push([\'_trackPageview\']);
-
-  (function() {
-    var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
-    ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
-    var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>';*/
-
-$head .= '
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id='.strip_tags($prm['head_ga_uaid']).'"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag(\'js\', new Date());
-
-  gtag(\'config\', \''.strip_tags($prm['head_ga_uaid']).'\', { \'anonymize_ip\': true, \'allow_display_features\': false });
-  
-</script>';
-		}
-
-		$set = false;
-		$buffer = JResponse::getBody();
-		if ($html != ''){
-			$buffer	= str_replace("<html ", "<html ". $html. " ", $buffer);
-			$set = true;
-		}
-		if ($head != ''){
-			$buffer = str_replace ("</head>", $head. "</head>", $buffer);
-			$set = true;
-		}
-		if ($body != ''){
-			$buffer = str_replace ("</body>", $body. "</body>", $buffer);
-			$set = true;
-		}
-		if ($set) {
-			JResponse::setBody($buffer);
-		}
 		return true;
 	}
 }
